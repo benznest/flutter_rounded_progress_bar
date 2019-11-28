4eed09020f139d8c6fff2e05480c7aab37e7717e
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2019-11-28 15:25:05 +0100

    Remove platform directories and dependencies

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mati/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mati/Projects/work/flutter_rounded_progress_bar/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mati/flutter/bin/cache/artifacts/engine/ios"
--- ios/Classes/FlutterRoundedProgressBarPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterRoundedProgressBarPlugin.h"
-#import <flutter_rounded_progress_bar/flutter_rounded_progress_bar-Swift.h>
-
-@implementation FlutterRoundedProgressBarPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterRoundedProgressBarPlugin registerWithRegistrar:registrar];
-}
-@end
