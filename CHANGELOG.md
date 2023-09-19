23f5d53dd9dfb94649ae0c392b832c468d9f8b09
benznest <benz7net@gmail.com>
2023-09-19 14:00:25 +0700

    update pubspec.yaml for publishing.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,14 @@
-##0.3.1
+## 0.3.2
+Fix pubspec.yaml
+
+## 0.3.1
 Migrate FlutterPlugin.
 
-##0.3.0
+## 0.3.0
 Migrate AndroidX.
 Add key param for RoundedProgressBar.
 
-##0.2.0
+## 0.2.0
 Migrate to null-safety
 
 ## 0.1.2
--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Works\flutter_rounded_progress_bar\example"
+export "FLUTTER_ROOT=/Users/benznest/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/benznest/Work/Flutter Project/flutter_rounded_progress_bar/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
